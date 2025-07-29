e9e7a188af19af8009d7bdfd0e532fce27e14be4
EmcoBih <bihoracemir513@gmail.com>
2025-07-29 22:27:57 +0200

    added my list files

---
+++ src/Alert.js
@@ -0,0 +1,27 @@
+// import React, { useEffect } from 'react';
+
+// const Alert = ({ type, msg, removeAlert, list }) => {
+//   useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       removeAlert();
+//     }, 3000);
+//     return () => clearTimeout(timeout);
+//   }, [list]);
+//   return <p className={`alert alert-${type}`}>{msg}</p>;
+// };
+
+// export default Alert;
+
+import React, { useEffect } from 'react';
+
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
